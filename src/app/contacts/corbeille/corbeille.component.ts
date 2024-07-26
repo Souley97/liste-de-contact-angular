663547a57fcdf696c475d1d9c44081b0d680a36b
souleymane97 <souleymane9700@gmail.com>
2024-07-26 12:46:48 +0000

    feat: creation des composents utilisateurs et contacts

---
+++ src/app/contacts/corbeille/corbeille.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corbeille',
+  standalone: true,
+  imports: [],
+  templateUrl: './corbeille.component.html',
+  styleUrl: './corbeille.component.css'
+})
+export class CorbeilleComponent {
+
+}
